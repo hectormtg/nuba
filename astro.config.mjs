df448aceba414886e0e548af691bc199f1fcdf5b
Hector Tamayo <hectormtg@gmail.com>
2025-09-12 10:55:50 -0400

    remove output parameter on astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,6 @@ import netlify from '@astrojs/netlify'
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
   integrations: [icon(), preact()],
   adapter: netlify(),
 })
