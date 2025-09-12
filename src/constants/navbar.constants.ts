89084df865c711e2615e18080d9f881101910cfd
Hector Tamayo <hectormtg@gmail.com>
2025-09-12 19:04:44 -0400

    tweak: check emails constant to show contact form

--- src/constants/navbar.constants.ts
+++ src/constants/navbar.constants.ts
@@ -1,8 +1,9 @@
 import type { NavbarOption } from '../types/navbar-option.types'
+import { SETTINGS } from './settings.constants'
 
 export const NAVBAR_OPTIONS: Array<NavbarOption> = [
   // { title: 'Home', elementId: '/', icon: 'home-icon' },
   { title: 'Nosotros', elementId: 'about', icon: 'user-icon' },
   { title: 'Servicios', elementId: 'services', icon: 'portfolio-icon' },
-  { title: 'Contacto', elementId: 'contact', icon: 'message-icon' },
-]
+  !SETTINGS.EMAILS_DISABLED && { title: 'Contacto', elementId: 'contact', icon: 'message-icon' },
+].filter(item => !!item)
--- src/constants/settings.constants.ts
+++ src/constants/settings.constants.ts
@@ -1,4 +1,4 @@
 export const SETTINGS = {
   MAINTENANCE_MODE: true,
-  EMAILS_DISABLED: false,
+  EMAILS_DISABLED: true,
 }
