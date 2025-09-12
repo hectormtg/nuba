8363a99a3b24791f2902b5ed0e2586afb7988947
Hector Tamayo <hectormtg@gmail.com>
2025-09-12 10:42:51 -0400

    change netlify adapter config

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,9 @@ import netlify from '@astrojs/netlify'
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   integrations: [icon(), preact()],
-  adapter: netlify(),
+  adapter: netlify({
+    edgeMiddleware: true,
+  }),
 })
